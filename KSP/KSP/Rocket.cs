2cbb464d9105c999d70d23626ae665c5d72acc69
creator-craft <37099826+creator-craft@users.noreply.github.com>
2021-06-01 12:53:08 +0200

    Add files via upload

---
+++ KSP/KSP/Rocket.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using KRPC.Client;
+using KRPC.Client.Services.SpaceCenter;
+
+class Rocket {
+    public Connection connection;
+    public Vessel vessel;
+
+    public double Head = 90;
+
+    public Rocket()
+    {
+        Console.WriteLine("Zogera is in startup");
+        connection = Program.connection;
+
+    }
+}
